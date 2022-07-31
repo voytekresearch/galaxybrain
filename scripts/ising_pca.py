c94c15a1d56db80f92c09a8aa4971f6cb79d7389
bfbarry <barry.brian.f@gmail.com>
2022-07-31 16:17:15 -0700

    e

--- scripts/ising_pca.py
+++ scripts/ising_pca.py
@@ -1,8 +1,10 @@
+import sys
+sys.path.append('..')
 from ..galaxybrain.ramsey import pca
 import h5py
 import numpy as np
 
-ising = h5py.File('../data/spikes/ising.hdf5', 'r') # keys := ['1.97', '2.07', '2.17', '2.27', '2.37', '2.47', '2.57']
+ising = h5py.File('/home/brirry/galaxybrain/data/spikes/ising.hdf5', 'r') # keys := ['1.97', '2.07', '2.17', '2.27', '2.37', '2.47', '2.57']
 PATH = '/home/brirry/ising_data'
 with h5py.File(f'{PATH}/ising_evals.hdf5', 'a') as f:
     for temp in ising:
