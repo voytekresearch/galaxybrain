44173f297ab87697b7deb9f7a43fc525f8a92261
bfbarry <barry.brian.f@gmail.com>
2023-03-26 11:39:14 -0700

    solved it

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os, sys
-sys.path.append(os.path.dirname(__file__))
+import sys
+import subprocess
+if 'sdsc' in subprocess.run('hostname', capture_output=True).stdout.decode('utf8'):
+    sys.path.append('/home/brirry/galaxybrain')
 from scripts.analysis_pipe import main
 
 if __name__ == '__main__':
